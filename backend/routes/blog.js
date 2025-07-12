30d9724c4a0e4e1c40be11dedb92095162398dff
MD MASOOM <128932048+MAS00M007@users.noreply.github.com>
2025-07-12 12:48:33 +0530

    Update blog.js

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -8,6 +8,7 @@ const {
 } = require('../controllers/blogController');
 const { auth } = require('../middleware/auth');
 const router = express.Router();
+//argoCD test
 
 router.post('/', auth, createBlog);
 router.get('/', auth, getUserBlogs);
